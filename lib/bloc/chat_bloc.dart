40c650791521eac0816035cd2f0c913afe351df8
malo.girard <malo.girard@jellysmack.com>
2023-12-31 15:56:24 +0100

    chore: wip stream messages

--- lib/bloc/chat_bloc.dart
+++ lib/bloc/chat_bloc.dart
@@ -3,6 +3,8 @@ import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rxdart/rxdart.dart';
+
 import '../models/contact_model.dart';
 import '../models/message_model.dart';
 
@@ -41,12 +43,15 @@ class ChatBloc extends Bloc<ChatEvent, ChatState> {
   final ContactModel contact;
   final userId = FirebaseAuth.instance.currentUser!.uid;
 
+  StreamSubscription? messagesSubscription;
+
   ChatBloc(this.contact) : super(ChatInitialState()) {
     // When chat is loading
     on<ChatLoading>((event, emit) async {
       try {
         final messages = await getMessages(userId, contact.id);
         emit(ChatLoadedState(messages));
+        add(ChatLoaded());
       } catch (e) {
         print(e);
       }
@@ -60,10 +65,33 @@ class ChatBloc extends Bloc<ChatEvent, ChatState> {
         print(e);
       }
     });
+
+    // When chat is loaded
+    on<ChatLoaded>((event, emit) async {
+      try {
+        // Start listening to messages
+        messagesSubscription?.cancel();
+        messagesSubscription = getStreamedMessages(userId, contact.id).listen((messages) {
+          if (messages != null) {
+            // ignore: invalid_use_of_visible_for_testing_member
+            emit(ChatLoadedState(messages as List<ChatMessageModel>));
+          }
+        });
+      } catch (e) {
+        print(e);
+      }
+    });
+    // messagesSubscription = getStreamedMessages(userId, contact.id).listen((messages) {
+    //   if (messages != null) {
+    //     // ignore: invalid_use_of_visible_for_testing_member
+    //     emit(ChatLoadedState(messages as List<ChatMessageModel>));
+    //   }
+    // });
   }
 
   // Get messages
-  Future<List<ChatMessageModel>> getMessages(String userId, String contactId) async {
+  Future<List<ChatMessageModel>> getMessages(
+      String userId, String contactId) async {
     try {
       var sendedMessages = await FirebaseFirestore.instance
           .collection('messages')
@@ -76,8 +104,10 @@ class ChatBloc extends Bloc<ChatEvent, ChatState> {
           .where('to', isEqualTo: userId)
           .get();
       var messages = <ChatMessageModel>[];
-      messages.addAll(sendedMessages.docs.map((e) => ChatMessageModel.fromDocument(e)));
-      messages.addAll(receivedMessages.docs.map((e) => ChatMessageModel.fromDocument(e)));
+      messages.addAll(
+          sendedMessages.docs.map((e) => ChatMessageModel.fromDocument(e)));
+      messages.addAll(
+          receivedMessages.docs.map((e) => ChatMessageModel.fromDocument(e)));
       messages.sort((a, b) => a.date.compareTo(b.date));
       return messages;
     } catch (e) {
@@ -98,5 +128,33 @@ class ChatBloc extends Bloc<ChatEvent, ChatState> {
     } catch (e) {
       print(e);
     }
+  }
+
+  // Recieve messages from stream
+  // NOTE: Need to be optimized to only get the last message and send event
+  Stream<List<ChatMessageModel?>> getStreamedMessages(
+      String userId, String contactId) async* {
+    var sendedMessages = FirebaseFirestore.instance
+        .collection('messages')
+        .where('from', isEqualTo: userId)
+        .where('to', isEqualTo: contactId)
+        .snapshots()
+        .map((event) =>
+            event.docs.map((e) => ChatMessageModel.fromDocument(e)).toList());
+    var receivedMessages = FirebaseFirestore.instance
+        .collection('messages')
+        .where('from', isEqualTo: contactId)
+        .where('to', isEqualTo: userId)
+        .snapshots()
+        .map((event) =>
+            event.docs.map((e) => ChatMessageModel.fromDocument(e)).toList());
+
+    var mergedMessages = Rx.merge([sendedMessages, receivedMessages]);
+
+    yield* mergedMessages.map((messages) {
+      messages.sort((a, b) => a.date.compareTo(b.date));
+      return messages;
+    });
+
   }
 }
